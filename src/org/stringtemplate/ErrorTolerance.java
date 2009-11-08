a716e9687e4ad835d2c2e2b03da9be9d4de4f381
parrt <parrt@antlr.org>
2009-11-07 16:11:50 -0800

    add new one
    [git-p4: depot-paths = "//depot/code/ST4/java/main/": change = 6324]

--- src/org/stringtemplate/ErrorTolerance.java
+++ src/org/stringtemplate/ErrorTolerance.java
@@ -4,7 +4,7 @@ public class ErrorTolerance {
 	// bit set values telling ST what to care about
 	public static final int DETECT_ADD_ATTR = 1;
 	public static final int DETECT_UNKNOWN_PROPERTY = 2;
-	public static final int DETECT_foo = 4;
+	public static final int DETECT_UNKNOWN_ATTRIBUTE = 4;
 
 	public static final int DEFAULT_TOLERANCE = DETECT_ADD_ATTR;
 
