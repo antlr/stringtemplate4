a1b223f3ca39959a7a542cac77e7af68ab5fb821
parrt <parrt@antlr.org>
2009-11-13 18:43:52 -0800

    fix comment.
    [git-p4: depot-paths = "//depot/code/ST4/java/main/": change = 6352]

--- src/org/stringtemplate/AttributeRenderer.java
+++ src/org/stringtemplate/AttributeRenderer.java
@@ -3,26 +3,14 @@ package org.stringtemplate;
 import java.util.Locale;
 
 /** This interface describes an object that knows how to format or otherwise
- *  render an object appropriately.  Usually this is used for locale changes
- *  for objects such as Date and floating point numbers...  You can either
- *  have an object that is sensitive to the locale or have a different object
- *  per locale.
+ *  render an object appropriately.  There is one renderer registered per
+ *  group for a given Java type.
  *
- *  Each template may have a renderer for each object type or can default
- *  to the group's renderer or the super group's renderer if the group doesn't
- *  have one.
- *
- *  The toString(Object,String) method is used when the user uses the
- *  format option: $o; format="f"$.  It checks the formatName and applies the
- *  appropriate formatting.  If the format string passed to the renderer is
- *  not recognized then simply call toString().
+ *  If the format string passed to the renderer is not recognized then simply
+ *  call toString().
  *
  *  formatString can be null but locale will at least be Locale.getDefault()
  */
 public interface AttributeRenderer {
-/*
-    public String toString(Object o);
-    public String toString(Object o, String formatString);
-     */
     public String toString(Object o, String formatString, Locale locale);
 }
