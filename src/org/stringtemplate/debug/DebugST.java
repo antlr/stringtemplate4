432e85ea1b544106badcb05781b0cbf7d735c837
parrt <parrt@antlr.org>
2009-12-21 14:10:15 -0800

    make it track attribute injection
    [git-p4: depot-paths = "//depot/code/ST4/java/main/": change = 6544]

--- src/org/stringtemplate/debug/DebugST.java
+++ src/org/stringtemplate/debug/DebugST.java
@@ -37,6 +37,7 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Locale;
 import java.io.StringWriter;
+import java.util.Map;
 
 /** To avoid polluting ST instances with debug info when not debugging.
  *  Setting debug mode in STGroup makes it create these instead of STs.
@@ -56,6 +57,17 @@ public class DebugST extends ST {
             addAttrEvents.map(name, new AddAttributeEvent(name, value));
         }
         super.add(name, value);
+    }
+
+    @Override
+    public void setAttributes(Map<String, Object> attributes) {
+        if ( groupThatCreatedThisInstance.debug ) {
+            for (String name : attributes.keySet()) {
+                Object value = attributes.get(name);
+                addAttrEvents.map(name, new AddAttributeEvent(name, value));
+            }
+        }
+        super.setAttributes(attributes);
     }
 
     // LAUNCH A WINDOW TO INSPECT TEMPLATE HIERARCHY
