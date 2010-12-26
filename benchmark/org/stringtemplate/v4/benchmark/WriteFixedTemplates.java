58c3e2d2942e12fe4484b4b79672546e940ceace
parrt <parrt@antlr.org>
2010-12-26 12:09:51 -0800

    mv template creation
    [git-p4: depot-paths = "//depot/code/ST4/java/main/": change = 7369]

--- benchmark/org/stringtemplate/v4/benchmark/WriteFixedTemplates.java
+++ benchmark/org/stringtemplate/v4/benchmark/WriteFixedTemplates.java
@@ -3,6 +3,14 @@ package org.stringtemplate.v4.benchmark;
 import org.stringtemplate.v4.ST;
 
 public class WriteFixedTemplates {
+	String bigTemplate;
+
+	public WriteFixedTemplates() {
+		StringBuilder buf = new StringBuilder();
+		for (int i=1; i<=1000; i++) buf.append("some text");
+		bigTemplate = buf.toString();
+	}
+
 	public void timeSingle(int reps) {
 		String template =
 			"A smallish string to write out";
@@ -13,10 +21,7 @@ public class WriteFixedTemplates {
 	}
 
 	public void timeSingleBigger(int reps) {
-		StringBuilder buf = new StringBuilder();
-		for (int i=1; i<=1000; i++) buf.append("some text");
-		String template = buf.toString();
-		ST st = new ST(template);
+		ST st = new ST(bigTemplate);
 		for (int i = 0; i < reps; i++) {
 			st.render();
 		}
