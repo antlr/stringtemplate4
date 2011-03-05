9dff17639cfcdbcd7320968d186fff3a7e9473e8
parrt <parrt@antlr.org>
2011-03-05 14:57:44 -0800

    NPE bug in format with 0 length string
    [git-p4: depot-paths = "//depot/code/ST4/java/main/": change = 7809]

--- src/org/stringtemplate/v4/StringRenderer.java
+++ src/org/stringtemplate/v4/StringRenderer.java
@@ -37,11 +37,11 @@ public class StringRenderer implements AttributeRenderer {
     // trim(s) and strlen(s) built-in funcs; these are format options
     public String toString(Object o, String formatString, Locale locale) {
         String s = (String)o;
-        if ( formatString==null ) return s; 
+        if ( formatString==null ) return s;
         if ( formatString.equals("upper") ) return s.toUpperCase(locale);
         if ( formatString.equals("lower") ) return s.toLowerCase(locale);
         if ( formatString.equals("cap") ) {
-            return Character.toUpperCase(s.charAt(0))+s.substring(1);
+            return (s.length() > 0) ? Character.toUpperCase(s.charAt(0))+s.substring(1) : s;
         }
         if ( formatString.equals("url-encode") ) {
             return URLEncoder.encode(s);
