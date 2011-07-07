16ac0656f48716d269c1b42228df4198c18e848a
parrt <parrt@antlr.org>
2011-07-07 10:14:55 -0800

    add unit test
    [git-p4: depot-paths = "//depot/code/ST4/java/main/": change = 8843]

--- test/org/stringtemplate/v4/test/TestRenderers.java
+++ test/org/stringtemplate/v4/test/TestRenderers.java
@@ -169,6 +169,22 @@ public class TestRenderers extends BaseTest {
 		assertEquals(expecting, result);
 	}
 
+	@Test public void testStringRendererWithSubtemplateInclude_cap() throws Exception {
+		// must toString the t() ref before applying format
+		String templates =
+				"foo(x) ::= << <{ack}; format=\"cap\"> >>\n";
+
+		writeFile(tmpdir, "t.stg", templates);
+		STGroup group = new STGroupFile(tmpdir+"/t.stg");
+		Interpreter.trace = true;
+		group.registerRenderer(String.class, new StringRenderer());
+		ST st = group.getInstanceOf("foo");
+		st.add("x", "hi");
+		String expecting = " Ack ";
+		String result = st.render();
+		assertEquals(expecting, result);
+	}
+
     @Test public void testStringRendererWithFormat_cap_emptyValue() throws Exception {
         String templates =
                 "foo(x) ::= << <x; format=\"cap\"> >>\n";
