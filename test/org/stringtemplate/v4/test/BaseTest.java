e6768db0b8331dced0712c04e9a7bbe7eab4d453
Sam Harwell <sam@tunnelvisionlabs.com>
2014-04-14 22:26:11 -0500

    Compile and build jar in-process

--- test/org/stringtemplate/v4/test/BaseTest.java
+++ test/org/stringtemplate/v4/test/BaseTest.java
@@ -34,10 +34,17 @@ import org.stringtemplate.v4.compiler.Compiler;
 import org.stringtemplate.v4.compiler.*;
 import org.stringtemplate.v4.misc.Misc;
 
+import javax.tools.JavaCompiler;
+import javax.tools.JavaFileObject;
+import javax.tools.StandardJavaFileManager;
+import javax.tools.ToolProvider;
 import java.io.*;
 import java.util.*;
+import java.util.jar.JarEntry;
+import java.util.jar.JarOutputStream;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 
 public abstract class BaseTest {
@@ -147,28 +154,93 @@ public abstract class BaseTest {
 		return exec(args, null, workingDirName);
 	}
 
-	public void jar(String fileName, String[] files, String workingDirName) {
-		String[] cmd = {
-			"jar", "cf", fileName
-		};
-		// SO SAD FOR YOU JAVA!!!!
-		List<String> list = new ArrayList<String>();
-		list.addAll(Arrays.asList(cmd));
-		list.addAll(Arrays.asList(files));
-		String[] a = new String[list.size()];
-		list.toArray(a);
-		exec(a, null, workingDirName); // create jar
+	public void jar(String fileName, String[] files, String workingDirName) throws IOException {
+		File workingDir = new File(workingDirName);
+		JarOutputStream stream = new JarOutputStream(new FileOutputStream(new File(workingDir, fileName)));
+		try {
+			for (String inputFileName : files) {
+				File file = new File(workingDirName, inputFileName);
+				addJarFile(file, workingDir, stream);
+			}
+		}
+		finally {
+			stream.close();
+		}
 	}
 
-	public void compile(String fileName, String workingDirName) {
-		String classpathOption = "-classpath";
+	protected void addJarFile(File file, File workingDir, JarOutputStream stream) throws IOException {
+		String name = workingDir.toURI().relativize(file.toURI()).getPath().replace("\\", "/");
+		if (file.isDirectory()) {
+			if (!name.isEmpty()) {
+				if (!name.endsWith("/")) {
+					// the entry for a folder must end with "/"
+					name += "/";
+				}
 
-		String[] args = new String[] {
-					"javac",
-					classpathOption, "."+pathSep+CLASSPATH,
-					fileName
-		};
-		exec(args, null, workingDirName);
+				JarEntry entry = new JarEntry(name);
+				entry.setTime(file.lastModified());
+				stream.putNextEntry(entry);
+				stream.closeEntry();
+			}
+
+			for (File child : file.listFiles()) {
+				addJarFile(child, workingDir, stream);
+			}
+
+			return;
+		}
+
+		JarEntry entry = new JarEntry(name);
+		entry.setTime(file.lastModified());
+		stream.putNextEntry(entry);
+
+		BufferedInputStream input = new BufferedInputStream(new FileInputStream(file));
+		try {
+			byte[] buffer = new byte[16 * 1024];
+			while (true) {
+				int count = input.read(buffer);
+				if (count == -1) {
+					break;
+				}
+
+				stream.write(buffer, 0, count);
+			}
+
+			stream.closeEntry();
+		}
+		finally {
+			input.close();
+		}
+	}
+
+	protected void compile(String fileName, String workingDirName) {
+		List<File> files = new ArrayList<File>();
+		files.add(new File(workingDirName, fileName));
+
+		JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+
+		StandardJavaFileManager fileManager =
+			compiler.getStandardFileManager(null, null, null);
+
+		Iterable<? extends JavaFileObject> compilationUnits =
+			fileManager.getJavaFileObjectsFromFiles(files);
+
+		Iterable<String> compileOptions =
+			Arrays.asList("-g", "-source", "1.6", "-target", "1.6", "-implicit:class", "-Xlint:-options", "-d", workingDirName, "-cp", workingDirName+pathSep+CLASSPATH);
+
+		JavaCompiler.CompilationTask task =
+			compiler.getTask(null, fileManager, null, compileOptions, null,
+							 compilationUnits);
+		boolean ok = task.call();
+
+		try {
+			fileManager.close();
+		}
+		catch (IOException ioe) {
+			ioe.printStackTrace(System.err);
+		}
+
+		assertTrue(ok);
 	}
 
 	public String exec(String[] args, String[] envp, String workingDirName) {
