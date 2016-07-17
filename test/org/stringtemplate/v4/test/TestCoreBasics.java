b631ab41d9f6cc60fe6b5aac29211b3c8cc78461
Sam Harwell <sam@tunnelvisionlabs.com>
2016-07-17 12:46:48 -0500

    Add test for ST.format
    Also remove incorrect examples from the documentation of this method.
    
    Fixes #113

--- test/org/stringtemplate/v4/test/TestCoreBasics.java
+++ test/org/stringtemplate/v4/test/TestCoreBasics.java
@@ -1064,4 +1064,12 @@ public class TestCoreBasics extends BaseTest {
 		st2.add("arg1", "value");
 		assertEquals("simple template", st2.render());
 	}
+
+	@Test public void testFormat_PositionalArguments() {
+		String n = "n";
+		String p = "p";
+		String expected = "n:p";
+		String actual = ST.format("<%1>:<%2>", n, p);
+		assertEquals(expected, actual);
+	}
 }
