3340cc2388b15dfe55e709f1ad5a490f0a6909d4
drealeed <drealeed2@yahoo.com>
2018-10-31 14:25:57 -0400

    Fix for issue #191 (importing templates in container classloaders)

--- src/org/stringtemplate/v4/misc/Misc.java
+++ src/org/stringtemplate/v4/misc/Misc.java
@@ -166,33 +166,24 @@ public class Misc {
 		return buf.toString();
 	}
 
+
 	public static boolean urlExists(URL url) {
 		try {
-			URLConnection connection = url.openConnection();
-			if (connection instanceof JarURLConnection) {
-				JarURLConnection jarURLConnection = (JarURLConnection)connection;
-				URLClassLoader urlClassLoader = new URLClassLoader(new URL[] { jarURLConnection.getJarFileURL() });
-				try {
-					return urlClassLoader.findResource(jarURLConnection.getEntryName()) != null;
-				}
-				finally {
-					if (urlClassLoader instanceof Closeable) {
-						((Closeable)urlClassLoader).close();
-					}
-				}
-			}
-
-			InputStream is = null;
+			// In Spring Boot context the URL can be like this:
+			// jar:file:/path/to/myapp.jar!/BOOT-INF/lib/mylib.jar!/org/foo/templates/g.stg
+			// This url cannot be processed using standard URLClassLoader
+			URLConnection con = url.openConnection();
+			InputStream is = con.getInputStream();
 			try {
-				is = url.openStream();
+				is.close();
+			} catch (Throwable e) {
+				// Closing the input stream may throw an exception. See bug below. Most probabaly it was
+				// the true reason for this commit: 
+				// https://github.com/antlr/stringtemplate4/commit/21484ed46f1b20b2cdaec49f9d5a626fb26a493c				
+				// https://bugs.openjdk.java.net/browse/JDK-8080094
+//				e.printStackTrace();
 			}
-			finally {
-				if (is != null) {
-					is.close();
-				}
-			}
-
-			return is != null;
+			return true;
 		}
 		catch (IOException ioe) {
 			return false;
